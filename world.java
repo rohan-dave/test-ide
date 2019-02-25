6859b93010765619d7fb1a51a47c622642c2dd85
Rohan Dave <rohandave31@gmail.com>
2019-02-25 13:40:04 -0800

    Created world.java

---
+++ world.java
@@ -0,0 +1,15 @@
+
+public class world {
+    public static void main(String[] args) {
+
+        System.out.println("Running from file: world");
+
+        System.out.println("Coursera's first integrated server side IDE");
+
+        int[] data = {1, 2, 3, 4, 5};
+        for (int number: data) {
+            System.out.println(number);
+        }
+    }
+
+}
